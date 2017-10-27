60721dcddf37b766b001da01b38b6156c71d1061
GiorgiB97 <giorgi.batiashvili@gmail.com>
2017-10-27 11:29:33 +0400

    design almost finished. this commit is for backup

---
+++ plugins/timer/timer.js
@@ -0,0 +1,29 @@
+// CHANGE DATE DOWN BELOW TO UPDATE THE TIMER
+var countDownDate = new Date("Nov 20, 2017 00:00:00").getTime();
+
+// TIMER.JS BY GEORGE BATIASHVILI
+var x = setInterval(function() {
+
+    var current = new Date().getTime();
+
+    var range = countDownDate - current;
+
+    var daysTo = Math.floor(range / (1000 * 60 * 60 * 24));
+    var hoursTo = Math.floor((range % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutesTo = Math.floor((range % (1000 * 60 * 60)) / (1000 * 60));
+    var secondsTo = Math.floor((range % (1000 * 60)) / 1000);
+
+    document.getElementById("daysTo").innerHTML = daysTo;
+    document.getElementById("hoursTo").innerHTML = hoursTo;
+    document.getElementById("minutesTo").innerHTML = minutesTo;
+    document.getElementById("secondsTo").innerHTML = secondsTo;
+
+    if (range < 0) {
+        clearInterval(x);
+        document.getElementById("daysTo").innerHTML = "00";
+        document.getElementById("hoursTo").innerHTML = "00";
+        document.getElementById("minutesTo").innerHTML = "00";
+        document.getElementById("secondsTo").innerHTML = "00";
+
+    }
+}, 1000);
